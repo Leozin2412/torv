427f041eabc5a57758a533d6613ead28f3b3faa5
Mateus Arturo <marturosantana@gmail.com>
2026-06-10 00:01:11 -0300

    correção multer

--- BackEndTorv/src/routes/profile.routes.js
+++ BackEndTorv/src/routes/profile.routes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const multer = require('multer');
 const path = require('path');
 const profileController = require('../controller/profile.controller');
 const authenticateToken = require('../middlewares/auth.middleware');
